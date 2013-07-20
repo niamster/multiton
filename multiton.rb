8fcb53c82b4be54030eeea5dc648b21d342e39f8
Dmytro Milinevskyy <milinevskyy@gmail.com>
2013-07-20 13:00:12 +0200

    initial implementation of Multiton pattern

---
+++ multiton.rb
@@ -0,0 +1,81 @@
+require "singleton"
+
+# The Multiton class extends Singleton module to collection level
+# The main purpose is to have a collection of sole instanses of a particular class
+# Possible application: a cache mapped to some DB
+#
+# == Usage
+#
+# To use Multiton, inherit it in your class
+#   class Klass < Multiton
+#     # ...
+#   end
+#
+# A single instance identified by some <ID> is created upon the first call of Klass.create(<ID>)
+# This ensures that there's only one instance of the Klass for a given <ID>
+#
+#   a, b = Klass.create(:id), Klass.create(:id)
+#   a == b
+#   # => true
+#   a.id == b.id
+#   # => true
+#
+# Before the call to Klass.initialize, instance variable @id is assigned to <ID> obtained from Klass.create
+#   class Klass < Multiton
+#     attr_reader :id
+#
+#     def initialize
+#       @ID = @id
+#     end
+#   end
+#
+#   a = Klass.create(:id)
+#   a.ID == a.id
+#   # => true
+#   a.ID == :id
+#   # => true
+#
+# The class Klass that inherits Multiton is final,
+# i.e. it's not possible to derive from a Klass anymore
+#
+class Multiton
+  include Singleton
+
+  attr_reader :id
+
+  class << Singleton
+    def __init__(klass)
+      klass.instance_eval {
+        @multiton__instances__ = {}
+        @singleton__mutex__ = Mutex.new
+      }
+
+      def klass.new(id, *args, &blk)
+        o = allocate
+        o.instance_variable_set(:@id, id)
+        o.instance_eval{initialize(*args, &blk)}
+        o
+      end
+      klass.private_class_method :new
+
+      def klass.instance
+        raise TypeError, "can't create a single instance of multiton #{self}"
+      end
+
+      def klass.inherited(sub)
+        raise TypeError, "can't inherit from of multiton class #{self.class}"
+      end
+
+      def klass.create(id)
+        return @multiton__instances__[id] if @multiton__instances__[id]
+        @singleton__mutex__.synchronize {
+          return @multiton__instances__[id] if @multiton__instances__[id]
+          @multiton__instances__[id] = new(id)
+        }
+        @multiton__instances__[id]
+      end
+
+      klass
+    end
+  end
+end
---
+++ spec/multiton_spec.rb
@@ -0,0 +1,77 @@
+require_relative '../multiton'
+
+describe Multiton do
+  before :each do
+    class A < Multiton
+    end
+  end
+
+  after :each do
+    Object.send(:remove_const, :A)
+    Object.send(:remove_const, :B) if defined? B
+  end
+
+  describe '#create' do
+    it "returns same instance of a class for given ID" do
+      a, b = A.create(:some_id), A.create(:some_id)
+      expect(a).to eq(b)
+    end
+
+    it "created instance survives GC work" do
+      a = A.create(:some_id)
+      a_oid = a.object_id
+      a = nil
+      GC.start
+      ObjectSpace.garbage_collect
+      a = A.create(:some_id)
+      expect(a_oid).to eq(a.object_id)
+    end
+
+    it "never collides on IDs for different classes" do
+      class B < Multiton
+      end
+      a, b = A.create(:some_id), B.create(:some_id)
+      expect(a).not_to eq(b)
+    end
+  end
+
+  it "creates instance for given ID on demand" do
+    expect(ObjectSpace.each_object(A){}).to eq(0)
+    A.create(:some_id)
+    expect(ObjectSpace.each_object(A){}).to eq(1)
+  end
+
+  it "impossible to inherit Multiton-derived class" do
+    expect {
+      class B < A
+      end
+    }.to raise_error
+  end
+
+  it "impossible to create an instance with Multiton.new" do
+    expect {
+      a = A.new :some_id
+    }.to raise_error
+  end
+
+  it "impossible to access #instance of included Singleton module" do
+    expect {
+      a = A.instance
+    }.to raise_error
+  end
+
+  describe '@id' do
+    it "instance variable @id is available before the call to #initialize" do
+      class B < Multiton
+        attr_reader :local_id
+        def initialize
+          @local_id = @id
+        end
+      end
+      b = B.create(:some_id)
+      expect(b.local_id).to eq(:some_id)
+      expect(b.id).to eq(:some_id)
+      expect(b.local_id).to eq(b.id)
+    end
+  end
+end
